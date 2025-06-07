94eb089146034904948fb319922890ce17a3e2cf
BaptistaJose <baptistaj133@gmail.com>
2025-06-06 23:05:33 -0300

    implementacion de cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import router from "./routes/indexRoutes";
 import morgan from "morgan";
+import cors from "cors";
 
 const server = express();
-
+server.use(cors())
 server.use(express.json());
 server.use(morgan("dev"));
 server.use(router);
